49db723d038808435ebbf652bac60de71150809f
kau12757 <kau12757@sheridancollege.ca>
2023-02-15 14:35:10 -0500

    Revert "rev1"
    This reverts commit 75d3d9a74880935a0a8c76e2096d94b81ba5d25b.

--- src/revision1/Revision1.java
+++ src/revision1/Revision1.java
@@ -17,7 +17,7 @@ public class Revision1 {
      */
     public static void main(String[] args) {
 Scanner sc=new Scanner(System.in);   //input from user
-String word=sc.next();  //kamal
+String word=sc.next();
 
 char[] rev=new char[word.length()];
 for(int i=0; i<word.length(); i++)      
